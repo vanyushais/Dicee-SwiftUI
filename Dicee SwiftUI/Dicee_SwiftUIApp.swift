335bd719d8ae78d93abe459343206ccb20aa5117
Ivan Istomin <139970052+vanyushais@users.noreply.github.com>
2023-08-30 15:52:28 +0300

    Add files via upload

---
+++ Dicee SwiftUI/Dicee_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SwiftUIApp.swift
+//  Dicee SwiftUI
+//
+//  Created by Ivan Istomin on 11.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
